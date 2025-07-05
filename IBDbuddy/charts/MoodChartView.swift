2d7700a0c859ad98cbd57139df621a350e36c761
agross25 <adinagross03@gmail.com>
2025-07-05 23:39:55 +0300

    added 3 charts

---
+++ IBDbuddy/charts/MoodChartView.swift
@@ -0,0 +1,58 @@
+//
+//  MoodChartView.swift
+//  IBDbuddy
+//
+//  Created by Adina on 7/5/25.
+//
+import SwiftUI
+import Charts
+
+struct MoodChartView: View {
+    let weekLogs: [DailyLog]
+    let formatter: DateFormatter
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Mood Rating:").font(.headline)
+            Chart {
+                ForEach(weekLogs) { log in
+                    LineMark(
+                        x: .value("Day", log.date, unit: .day),
+                        y: .value("Mood", log.moodRating)
+                    )
+                    .foregroundStyle(Color(red: 1.0, green: 0.75, blue: 0.55))
+                    .interpolationMethod(.catmullRom)
+
+                    PointMark(
+                        x: .value("Day", log.date, unit: .day),
+                        y: .value("Mood", log.moodRating)
+                    )
+                    .foregroundStyle(Color(red: 1.0, green: 0.75, blue: 0.55))
+                    .symbolSize(50)
+                }
+            }
+            .chartYAxis {
+                AxisMarks(position: .leading)
+            }
+            .chartXAxis {
+                AxisMarks(values: .stride(by: .day)) { value in
+                    AxisGridLine()
+                    AxisTick()
+                    AxisValueLabel {
+                        if let date = value.as(Date.self) {
+                            let isToday = Calendar.current.isDateInToday(date)
+                            return Text(formatter.string(from: date))
+                                .fontWeight(isToday ? .bold : .regular)
+                        } else {
+                            return Text("")
+                        }
+                    }
+                }
+            }
+            .frame(height: 120)
+        }
+        .padding(.horizontal, 40)
+        .padding(.vertical, 10)
+        .border(.gray, width: 3)
+    }
+}
